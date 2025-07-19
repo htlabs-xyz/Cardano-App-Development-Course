3bf5b2d00898e8b567a4125780c902a1b59091dd
Phùng Tiến Dũng <tiendung0325@gmail.com>
2025-07-20 01:38:23 +0700

    Update route.ts

--- Code/Bai2/src/app/(server)/api/tasks/[id]/route.ts
+++ Code/Bai2/src/app/(server)/api/tasks/[id]/route.ts
@@ -1,9 +1,10 @@
 import { NextResponse } from 'next/server';
-import { tasks } from '../tasks';
+import { tasks } from '../data';
 
 
-export async function GET({ params }: { params: { id: string } }) {
-    const task = tasks.find((t: { id: number }) => t.id === Number(params.id));
+export async function GET(request: Request, { params }: { params: Promise<{ id: string }> }) {
+    const { id } = await params
+    const task = tasks.find((t: { id: number }) => t.id === Number(id));
     if (!task) {
         return NextResponse.json({ error: 'Task not found' }, { status: 404 });
     }
